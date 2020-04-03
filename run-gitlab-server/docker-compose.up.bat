440ef550d8d73abf3842e0f9f7a51e8b8ddd30a0
Geir <jerstad_geir_ivar@hotmail.com>
2020-04-03 16:27:19 +0200

    ..

--- run-gitlab-server/docker-compose.up.bat
+++ run-gitlab-server/docker-compose.up.bat
@@ -1,8 +1,9 @@
 cd %~dp0
-docker rm -f learning_gitlab-1
+docker rm -f gitlab_ee-1
+docker rm -f gitlab_runner1-1
 docker-compose -f docker-compose.yml down --remove-orphans
-docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml pull --no-parallel
 docker-compose -f docker-compose.yml up -d --remove-orphans
 REM wait for 1-2 seconds for the container to start
 pause
-docker exec -it learning_gitlab-1 /bin/bash
+docker exec -it gitlab_runner1-1 /bin/bash
---
+++ run-gitlab-server/runner-logs.bat
@@ -0,0 +1,1 @@
+docker logs -f gitlab_runner1-1
