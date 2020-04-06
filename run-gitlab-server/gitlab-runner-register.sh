d1e94cb65f0c07fb8d203a03487c9e4f675321a6
Geir <jerstad_geir_ivar@hotmail.com>
2020-04-06 15:10:34 +0000

    gitlab_runner_register_cli

--- run-gitlab-server/gitlab-runner-register.sh
+++ run-gitlab-server/gitlab-runner-register.sh
@@ -14,4 +14,5 @@ docker exec -it gitlab_runner1 \
     --executor docker \
     --docker-image docker:stable \
     --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
-    --docker-network-mode gitlab-network
+    --docker-network-mode gitlab-network \
+    --ssh_port 22
